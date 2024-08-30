7bd3ecd5965aff8692b7ad05f51329726eb7e2e9
Supermarcel10 <marcelbarlik@gmail.com>
2024-08-30 23:10:54 +0100

    - Updated Company fields to be public.

--- src/structs/company.rs
+++ src/structs/company.rs
@@ -1,10 +1,10 @@
 use crate::{impl_attachable, impl_base};
 
 pub struct Company {
-	name : String,
-	location : Option<String>,
-	industry : Option<String>,
-	company_type : CompanyType
+	pub name : String,
+	pub location : Option<String>,
+	pub industry : Option<String>,
+	pub company_type : CompanyType
 }
 
 pub enum CompanyType {
